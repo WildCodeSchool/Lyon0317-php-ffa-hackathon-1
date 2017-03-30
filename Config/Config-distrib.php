ea3d6bac6f6e4edbea9f3be5d77c3ef117d8d668
Florence Bouvier <florencebres1@gmail.com>
2017-03-30 17:05:08 +0200

    add BddManager

---
+++ Config/Config-distrib.php
@@ -0,0 +1,9 @@
+<?php
+namespace  wcs;
+class Config
+{
+    const USER      = "";
+    const PASSWORD  = "";
+    const HOST      = "";
+    const DBNAME    = "";
+}
--- src/bddManager.php
+++ src/bddManager.php
@@ -1,7 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Alex
- * Date: 30/03/2017
- * Time: 10:42
- */
+namespace hack;
+class BddManager
+{
+    /**
+     * @var \mysqli
+     */
+    private $connection;
+    public function __construct()
+    {
+        $mysqli = new \mysqli(Config::HOST, Config::USER, Config::PASSWORD, Config::DBNAME);
+        if ($mysqli->connect_errno) {
+            throw new \mysqli_sql_exception(
+                "Failed to connect to MySQL : (" .
+                $mysqli->connect_errno . ") " .
+                $mysqli->connect_error
+            );
+        }
+        $this->connection = $mysqli;
+    }
+    /**
+     * @return \mysqli
+     */
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+    /**
+     * @param string $sql
+     * @return bool|\mysqli_result
+     */
+    public function execSql($sql)
+    {
+        if (!$result = $this->connection->query($sql)) {
+            throw new \mysqli_sql_exception(
+                "Failed to run query : (" .
+                $this->connection->connect_errno . ") " .
+                $this->connection->connect_error
+            );
+        }
+        return $result;
+    }
+}
